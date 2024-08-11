c16c06568eb7179dec9a4b2dc8959500d681993e
SARAN S M <saran007mgp@gmail.com>
2024-08-11 22:38:04 +0530

    bug fixing

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ const port = process.env.PORT || 5000;
 const dbConnect = require('./dbConnect');
 dbConnect();
 
+
+
+// const http = require('http');
+
+const app = require('./src/app.js');
+
 app.use(cors({
     origin: ['http://localhost:3000', 'https://bit-special-lab-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PUT'],
     credentials: false,
 }));
-
-// const http = require('http');
-
-const app = require('./src/app.js');
 
 // import app from './src/app.js';
 
